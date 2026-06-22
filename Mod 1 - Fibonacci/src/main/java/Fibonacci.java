3ebddec74298c17140848b2a27d4d8fd70a0b77b
Todd Uhl <uhlto13@gmail.com>
2026-06-21 21:42:41 -0400

    Updated output message for jenkins

--- Mod 1 - Fibonacci/src/main/java/Fibonacci.java
+++ Mod 1 - Fibonacci/src/main/java/Fibonacci.java
@@ -20,7 +20,7 @@ public class Fibonacci {
 
         int answer = fibonacci(term);
 
-        System.out.println("Modified code and rebuilt: The " + term
+        System.out.println("Modified code for Jenkins and rebuilt: The " + term
                 + "th term of the Fibonacci sequence is "
                 + answer + ".");
     }
